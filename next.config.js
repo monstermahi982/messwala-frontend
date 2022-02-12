5804fc70e44c799c2e4f52707ece5adcdff80446
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-12 16:07:31 +0530

    added image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['source.unsplash.com'],
+    domains: ['source.unsplash.com', 'messwala-test8778.s3.amazonaws.com', 'messwala-test8778.s3.us-east-2.amazonaws.com'],
   },
 
 }
