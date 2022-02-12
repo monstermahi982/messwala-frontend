85a161e4955a4a8790748764bd73292061206a83
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-12 15:54:52 +0530

    addedbackend and alert

--- components/UpdateInfo.js
+++ components/UpdateInfo.js
@@ -9,10 +9,16 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
+import axios from 'axios';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const UpdateInfo = ({ data }) => {
-
+    const URL = process.env.NEXT_PUBLIC_URL;
     const [lunch, setLunch] = React.useState("12:30");
     const [dinner, setDinner] = React.useState("19:30");
     const [name, setName] = React.useState(data.mess_name);
@@ -21,13 +27,43 @@ const UpdateInfo = ({ data }) => {
     const [nonveg, setNonveg] = React.useState(data.non_veg);
     const [parcel, setParcel] = React.useState(data.parcel_service);
 
-    const updateMess = () => {
-        console.log(lunch, dinner);
+    const updateMess = async () => {
+
+        const mess = {
+            mess_name: name,
+            mess_address: address,
+            thali_price: price,
+            non_veg: nonveg,
+            parcel_service: parcel,
+            lunch_time: lunch,
+            dinner_time: dinner
+        }
+        const updateData = await axios.put(`${URL}mess/info/${data._id}`, mess);
+        if (updateData.data === "info updated") {
+            setAlertMessage({ message: updateData.data, status: "success" });
+            setAlert(true);
+        }
     }
+
+    const [alert, setAlert] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState({});
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setAlert(false);
+    };
 
     return (
         <>
             <Box sx={{ py: 1, px: 4 }}>
+                <Snackbar open={alert} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity={alertMessage.status} sx={{ width: '100%' }}>
+                        {alertMessage.message}
+                    </Alert>
+                </Snackbar>
                 <Typography sx={{ textAlign: 'center', fontSize: '30px', letterSpacing: '3px', py: 2 }}>Update Informations</Typography>
                 <TextField sx={{ my: 1 }} id="name" value={name} onChange={(e) => setName(e.target.value)} fullWidth label="Mess Name" variant="outlined" />
                 <TextField sx={{ my: 1 }} id="address" value={address} onChange={(e) => setAddress(e.target.value)} fullWidth label="Mess Address" variant="outlined" />
