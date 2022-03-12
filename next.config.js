5169f57b873a4acc42202492e82d80c2d240eb1e
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-03-13 00:59:21 +0530

    Added pwa feature

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['source.unsplash.com', 'messwala-test8778.s3.amazonaws.com', 'messwala-test8778.s3.us-east-2.amazonaws.com'],
   },
 
-}
+})
